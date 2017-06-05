7581b4a8e9035fadf8ee8ea1d4f965ffb38abcee
Msingh91 <manishsingh@Manishs-MacBook-Pro.local>
2017-06-05 16:26:30 -0500

    ok

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var friends = [{
+	name: "Danny",
+	link: "http://google.com",
+	scores:[4,2,1,2,3]
+}];
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -20,8 +26,17 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "survey.html"));
 });
 
+app.get("/api/friends", function(req,res) {
+	return res.json(friends)
 
+});
 
+app.post("/api/new", function(req,res) {
+	var newFriend = req.body
+	friends.push(newFriend)
+	console.log(newFriend)
+
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
